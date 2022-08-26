8cfec0ed521c8f91de3278a8588fb17e6e1505c7
Derek <alexanderderekrein@gmail.com>
2022-08-26 17:50:01 -0400

    feat: support L2s

--- src/env.rs
+++ src/env.rs
@@ -29,11 +29,23 @@ fn default_log_level() -> String {
 
 fn default_infura_supported_chains() -> HashMap<String, String> {
     HashMap::from([
+        // Ethereum
         ("eip155:1".into(), "mainnet".into()),
         ("eip155:3".into(), "ropsten".into()),
         ("eip155:42".into(), "kovan".into()),
         ("eip155:4".into(), "rinkeby".into()),
         ("eip155:5".into(), "goerli".into()),
+        // Polygon
+        ("eip155:137".into(), "polygon-mainnet".into()),
+        ("eip155:80001".into(), "polygon-mumbai".into()),
+        // Optimism
+        ("eip155:10".into(), "optimism-mainnet".into()),
+        ("eip155:69".into(), "optimism-kovan".into()),
+        ("eip155:420".into(), "optimism-goerli".into()),
+        // Arbitrum
+        ("eip155:42161".into(), "arbitrum-mainnet".into()),
+        ("eip155:421611".into(), "arbitrum-rinkeby".into()),
+        ("eip155:421613".into(), "arbitrum-goerli".into()),
     ])
 }
 
