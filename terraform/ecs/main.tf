80f111dfe1d118e2ab98ceaead78d60361b167d6
Derek <alexanderderekrein@gmail.com>
2022-11-18 09:32:25 -0500

    chore: improvements

--- terraform/ecs/main.tf
+++ terraform/ecs/main.tf
@@ -14,7 +14,7 @@ locals {
   // TODO: version the RPC image so we can pin it
   # pinned_latest_tag     = sort(setsubtract(data.aws_ecr_image.service_image.image_tags, ["latest"]))[0]
   // TODO: allow caller to pin version
-  image_tag = "latest" # TODO: var.ecr_app_version == "latest" ? local.pinned_latest_tag : var.ecr_app_version
+  image_tag = data.aws_ecr_image.service_image.image_tags[0] # TODO: var.ecr_app_version == "latest" ? local.pinned_latest_tag : var.ecr_app_version
   image     = "${var.ecr_repository_url}:${local.image_tag}"
 }
 
