a4fa76f8ae9ad691e90c318cb567407995ca8766
Rakowskiii <raku@walletconnect.com>
2022-12-20 13:40:35 +0100

    Split tests in gh actions
    Signed-off-by: Rakowskiii <raku@walletconnect.com>

--- tests/functional/connection/mod.rs
+++ tests/functional/connection/mod.rs
@@ -1,5 +1,5 @@
 pub(crate) mod binance;
-// pub(crate) mod infura;
-// pub(crate) mod pokt;
+pub(crate) mod infura;
+pub(crate) mod pokt;
 
 const INFURA_CHAIN_DECOMISSIONED_ERROR_CODE: i32 = -32601;
