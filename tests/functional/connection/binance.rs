ca562761fe2b9618aff1656d0aa4039644e759a8
Rakowskiii <raku@walletconnect.com>
2022-12-20 13:33:50 +0100

    Testing with just binance

--- tests/functional/connection/binance.rs
+++ tests/functional/connection/binance.rs
@@ -1,78 +1,61 @@
-use hyper::{Body, Client, Method, Request, StatusCode};
+use hyper::{Client, StatusCode};
 use hyper_tls::HttpsConnector;
 use test_context::test_context;
 
 use crate::{context::ServerContext, utils::send_jsonrpc_request, JSONRPC_VERSION};
 
-// #[test_context(ServerContext)]
-// #[tokio::test]
-// async fn eip155_56_bsc_mainnet(ctx: &mut ServerContext) {
-//     let addr = format!(
-//         "http://{}/v1?projectId={}&chainId=",
-//         ctx.server.public_addr, ctx.server.project_id
-//     );
-
-//     let client = Client::builder().build::<_, hyper::Body>(HttpsConnector::new());
-//     let request = jsonrpc::Request {
-//         method: "eth_chainId",
-//         params: &[],
-//         id: serde_json::Value::Number(1.into()),
-//         jsonrpc: JSONRPC_VERSION,
-//     };
-
-//     let (status, rpc_response) = send_jsonrpc_request(client, addr, "eip155:56", request).await;
-
-//     // Verify that HTTP communication was successful
-//     assert_eq!(status, StatusCode::OK);
-
-//     // Verify there was no error in rpc
-//     assert!(rpc_response.error.is_none());
-
-//     // Check chainId
-//     assert_eq!(rpc_response.result::<String>().unwrap(), "0x38")
-// }
-
-// #[test_context(ServerContext)]
-// #[tokio::test]
-// async fn eip155_97_bsc_testnet(ctx: &mut ServerContext) {
-//     let addr = format!(
-//         "http://{}/v1?projectId={}&chainId=",
-//         ctx.server.public_addr, ctx.server.project_id
-//     );
-
-//     let client = Client::builder().build::<_, hyper::Body>(HttpsConnector::new());
-//     let request = jsonrpc::Request {
-//         method: "eth_chainId",
-//         params: &[],
-//         id: serde_json::Value::Number(1.into()),
-//         jsonrpc: JSONRPC_VERSION,
-//     };
-
-//     let (status, rpc_response) = send_jsonrpc_request(client, addr, "eip155:97", request).await;
-
-//     // Verify that HTTP communication was successful
-//     assert_eq!(status, StatusCode::OK);
-
-//     // Verify there was no error in rpc
-//     assert!(rpc_response.error.is_none());
-
-//     // Check chainId
-//     assert_eq!(rpc_response.result::<String>().unwrap(), "0x61")
-// }
 #[test_context(ServerContext)]
 #[tokio::test]
-async fn health_check_2(ctx: &mut ServerContext) {
-    let addr = format!("http://{}/health", ctx.server.public_addr);
+async fn eip155_56_bsc_mainnet(ctx: &mut ServerContext) {
+    let addr = format!(
+        "http://{}/v1?projectId={}&chainId=",
+        ctx.server.public_addr, ctx.server.project_id
+    );
 
     let client = Client::builder().build::<_, hyper::Body>(HttpsConnector::new());
+    let request = jsonrpc::Request {
+        method: "eth_chainId",
+        params: &[],
+        id: serde_json::Value::Number(1.into()),
+        jsonrpc: JSONRPC_VERSION,
+    };
 
-    let request = Request::builder()
-        .method(Method::GET)
-        .uri(addr)
-        .body(Body::default())
-        .unwrap();
+    let (status, rpc_response) = send_jsonrpc_request(client, addr, "eip155:56", request).await;
 
-    let response = client.request(request).await.unwrap();
+    // Verify that HTTP communication was successful
+    assert_eq!(status, StatusCode::OK);
 
-    assert_eq!(response.status(), StatusCode::OK)
+    // Verify there was no error in rpc
+    assert!(rpc_response.error.is_none());
+
+    // Check chainId
+    assert_eq!(rpc_response.result::<String>().unwrap(), "0x38")
+}
+
+#[test_context(ServerContext)]
+#[tokio::test]
+async fn eip155_97_bsc_testnet(ctx: &mut ServerContext) {
+    let addr = format!(
+        "http://{}/v1?projectId={}&chainId=",
+        ctx.server.public_addr, ctx.server.project_id
+    );
+
+    let client = Client::builder().build::<_, hyper::Body>(HttpsConnector::new());
+    let request = jsonrpc::Request {
+        method: "eth_chainId",
+        params: &[],
+        id: serde_json::Value::Number(1.into()),
+        jsonrpc: JSONRPC_VERSION,
+    };
+
+    let (status, rpc_response) = send_jsonrpc_request(client, addr, "eip155:97", request).await;
+
+    // Verify that HTTP communication was successful
+    assert_eq!(status, StatusCode::OK);
+
+    // Verify there was no error in rpc
+    assert!(rpc_response.error.is_none());
+
+    // Check chainId
+    assert_eq!(rpc_response.result::<String>().unwrap(), "0x61")
 }
