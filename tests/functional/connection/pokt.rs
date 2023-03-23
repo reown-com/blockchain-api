1ae88a3f9db1a0fc65cd726e6bdc36413feff404
Rakowskiii <maciejrak@protonmail.com>
2023-03-23 12:32:59 +0100

    fix: restore solana test after Pokt fix

--- tests/functional/connection/pokt.rs
+++ tests/functional/connection/pokt.rs
@@ -61,37 +61,36 @@ async fn eip155_100_gnosis(ctx: &mut ServerContext) {
     assert_eq!(rpc_response.result::<String>().unwrap(), "0x64")
 }
 
-// TODO: find new provider for solana - POKT currently is throwing internal
-// server error for this #[test_context(ServerContext)]
-// #[tokio::test]
-// async fn solana_mainnet(ctx: &mut ServerContext) {
-//     let addr = format!(
-//         "http://{}/v1?projectId={}&chainId=",
-//         ctx.server.public_addr, ctx.server.project_id
-//     );
+#[test_context(ServerContext)]
+#[tokio::test]
+async fn solana_mainnet(ctx: &mut ServerContext) {
+    let addr = format!(
+        "http://{}/v1?projectId={}&chainId=",
+        ctx.server.public_addr, ctx.server.project_id
+    );
 
-//     let client = Client::builder().build::<_,
-// hyper::Body>(HttpsConnector::new());     let request = jsonrpc::Request {
-//         method: "getHealth",
-//         params: &[],
-//         id: serde_json::Value::Number(1.into()),
-//         jsonrpc: JSONRPC_VERSION,
-//     };
+    let client = Client::builder().build::<_, hyper::Body>(HttpsConnector::new());
+    let request = jsonrpc::Request {
+        method: "getHealth",
+        params: &[],
+        id: serde_json::Value::Number(1.into()),
+        jsonrpc: JSONRPC_VERSION,
+    };
 
-//     let (status, rpc_response) = send_jsonrpc_request(
-//         client,
-//         addr,
-//         "solana:4sgjmw1sunhzsxgspuhpqldx6wiyjntz",
-//         request,
-//     )
-//     .await;
+    let (status, rpc_response) = send_jsonrpc_request(
+        client,
+        addr,
+        "solana:4sgjmw1sunhzsxgspuhpqldx6wiyjntz",
+        request,
+    )
+    .await;
 
-//     // Verify that HTTP communication was successful
-//     assert_eq!(status, StatusCode::OK);
+    // Verify that HTTP communication was successful
+    assert_eq!(status, StatusCode::OK);
 
-//     // Verify there was no error in rpc
-//     assert!(rpc_response.error.is_none());
+    // Verify there was no error in rpc
+    assert!(rpc_response.error.is_none());
 
-//     // Check chainId
-//     assert_eq!(rpc_response.result::<String>().unwrap(), "ok")
-// }
+    // Check chainId
+    assert_eq!(rpc_response.result::<String>().unwrap(), "ok")
+}
