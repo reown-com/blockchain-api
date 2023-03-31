f3a5b1118738b74e4e892b78b130b7864804d0b4
Rakowskiii <maciejrak@protonmail.com>
2023-04-01 05:44:56 +0900

    fix: terraform fmt

--- terraform/monitoring/main.tf
+++ terraform/monitoring/main.tf
@@ -13,7 +13,7 @@ locals {
   opsgenie_notification_channel = "l_iaPw6nk"
   notifications = (
     var.environment == "prod" ?
-    [{"uid": "${local.opsgenie_notification_channel}"}] :
+    [{ "uid" : "${local.opsgenie_notification_channel}" }] :
     []
   )
 
