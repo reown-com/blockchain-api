14e0801d5bfd5dfad667b728e1515a633013b7df
Rakowskiii <maciejrak@protonmail.com>
2023-05-22 19:26:52 +0200

    fix: revet "fix: tflint"
    This reverts commit d523e8fa6f6c8fa863a1aaae74046abc3a0a2caa.

--- terraform/monitoring/variables.tf
+++ terraform/monitoring/variables.tf
@@ -6,3 +6,14 @@ variable "prometheus_workspace_id" {
   type = string
 }
 
+variable "redis_cluster_id" {
+  type = string
+}
+
+variable "target_group_arn" {
+  type = string
+}
+
+variable "load_balancer_arn" {
+  type = string
+}
