4810416e870c599dd80f58568d18366f8d415c02
Rakowskiii <maciejrak@protonmail.com>
2023-06-07 22:50:54 +0200

    fix: decrease binance weight as it's failing

--- src/env/binance.rs
+++ src/env/binance.rs
@@ -28,7 +28,7 @@ fn default_supported_chains() -> HashMap<String, (String, Weight)> {
         // Binance Smart Chain Mainnet
         (
             "eip155:56".into(),
-            ("https://bsc-dataseed.binance.org/".into(), Weight(9.into())),
+            ("https://bsc-dataseed.binance.org/".into(), Weight(6.into())),
         ),
         // Binance Smart Chain Testnet
         (
