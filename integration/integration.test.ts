13bdaaae9590e080b8538c590466bbba8ed0040e
Derek <alexanderderekrein@gmail.com>
2023-11-21 00:09:11 +0100

    feat: enable test

--- integration/integration.test.ts
+++ integration/integration.test.ts
@@ -12,11 +12,21 @@ declare let process: {
 }
 
 describe('blockchain api', () => {
+  let baseUrl: string
+  let projectId: string
+  beforeAll(() => {
+    baseUrl = process.env.RPC_URL
+    if (!baseUrl) {
+      throw new Error('RPC_URL environment variable not set')
+    }
+    projectId = process.env.PROJECT_ID
+    if (!projectId) {
+      throw new Error('PROJECT_ID environment variable not set')
+    }
+  })
   describe('Health', () => {
-    const url = `${process.env.RPC_URL}/health`
-
     it('is healthy', async () => {
-      const { status } = await http.get(`${url}`)
+      const { status } = await http.get(`${baseUrl}/health`)
 
       expect(status).toBe(200)
     })
@@ -24,13 +34,17 @@ describe('blockchain api', () => {
   describe('Identity', () => {
     it('known ens', async () => {
       let resp: any = await http.get(
-        `${process.env.RPC_URL}/v1/identity/0xf3ea39310011333095CFCcCc7c4Ad74034CABA63?chainId=eip155%3A1&projectId=${process.env.PROJECT_ID}`,
+        `${baseUrl}/v1/identity/0xf3ea39310011333095CFCcCc7c4Ad74034CABA63?chainId=eip155%3A1&projectId=${projectId}`,
       )
-      // TODO: uncomment when API works again
-      // expect(resp.status).toBe(200)
-      // expect(resp.data.origin).toBe('http://app.uniswap.org')
-      // expect(resp.data.isScam).toBe(false)
-      // expect(resp.headers["access-control-allow-origin"]).toBe("*")
+      expect(resp.status).toBe(200)
+      expect(resp.data.name).toBe('cyberdrk.eth')
+    })
+    it('unknown ens', async () => {
+      let resp: any = await http.get(
+        `${baseUrl}/v1/identity/0xf3ea39310011333095CFCcCc7c4Ad74034CABA64?chainId=eip155%3A1&projectId=${projectId}`,
+      )
+      expect(resp.status).toBe(200)
+      expect(resp.data.name).toBe(null)
     })
   })
 })
