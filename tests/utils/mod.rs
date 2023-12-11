395523630c58cb1ae71d31e1804fee33fda2c95a
Max Kalashnikoff <geekmaks@gmail.com>
2023-12-11 23:27:37 +0100

    feat: implement helpers

--- tests/utils/mod.rs
+++ tests/utils/mod.rs
@@ -2,6 +2,9 @@ use {
     axum::http::HeaderValue,
     hyper::{body, client::HttpConnector, Body, Client, Method, Request, StatusCode},
     hyper_tls::HttpsConnector,
+    rand::{distributions::Alphanumeric, Rng},
+    sqlx::{postgres::PgPoolOptions, PgPool},
+    std::env,
 };
 
 pub async fn send_jsonrpc_request(
@@ -39,4 +42,28 @@ pub async fn send_jsonrpc_request(
             )
         }),
     )
+}
+
+pub async fn get_postgres_pool() -> PgPool {
+    let postgres = PgPoolOptions::new()
+        .connect(&env::var("RPC_PROXY_POSTGRES_URI").unwrap())
+        .await
+        .unwrap();
+    sqlx::migrate!("./migrations").run(&postgres).await.unwrap();
+    postgres
+}
+
+pub fn generate_random_string(len: usize) -> String {
+    let rng = rand::thread_rng();
+    rng.sample_iter(&Alphanumeric)
+        .filter_map(|b| {
+            let c = b as char;
+            if c.is_ascii_alphanumeric() && c.is_ascii_lowercase() || c.is_ascii_digit() {
+                Some(c)
+            } else {
+                None
+            }
+        })
+        .take(len)
+        .collect()
 }
