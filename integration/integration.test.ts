e8352ddcdb36222c2fffd8089260d651de9863f6
Max Kalashnikoff <geekmaks@gmail.com>
2024-01-09 13:09:17 +0100

    feat(tests): adding integration tests

--- integration/integration.test.ts
+++ integration/integration.test.ts
@@ -239,4 +239,40 @@ describe('blockchain api', () => {
       expect(first_address.address).toBe(address)
     })
   })
+
+  describe('Generators', () => {
+    it('onramp Pay SDK URL', async () => {
+      const expected_host = 'https://pay.coinbase.com/buy/select-asset';
+      const address = '0x1234567890123456789012345678901234567890';
+      const partnerUserId = 'someUserID';
+      const payload = {
+        partnerUserId,
+        destinationWallets:[{ address }],
+      };
+      let resp: any = await http.post(
+        `${baseUrl}/v1/generators/onrampurl?projectId=${projectId}`,
+        payload
+      )
+      expect(resp.status).toBe(200)
+      expect(typeof resp.data).toBe('object')
+      expect(typeof resp.data.url).toBe('string')
+      expect(resp.data.url).toContain(expected_host)
+      expect(resp.data.url).toContain(address)
+      expect(resp.data.url).toContain(partnerUserId)
+    })
+    it('onramp Pay SDK URL wrong payload', async () => {
+      const address = '0x1234567890123456789012345678901234567890';
+      const partnerUserId = 'someUserID';
+      // Creating the wrong payload
+      const payload = {
+        partner: partnerUserId,
+        someWallets:[{ address }],
+      };
+      let resp: any = await http.post(
+        `${baseUrl}/v1/generators/onrampurl?projectId=${projectId}`,
+        payload
+      )
+      expect(resp.status).toBe(400)
+    })
+  })
 })
