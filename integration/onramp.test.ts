8f4b0ddfb00e5fa1d974ed88f07502eb89749f33
Max Kalashnikoff <geekmaks@gmail.com>
2024-02-19 12:48:07 +0100

    feat: adding integration test

--- integration/onramp.test.ts
+++ integration/onramp.test.ts
@@ -3,10 +3,12 @@ import { getTestSetup } from './init';
 describe('OnRamp', () => {
   const { baseUrl, projectId, httpClient } = getTestSetup();
   const onRampPath = `${baseUrl}/v1/onramp`;
+  const country = 'US';
+  const subdivision = 'NY';
 
   it('buy options', async () => {
     let resp: any = await httpClient.get(
-      `${onRampPath}/buy/options?projectId=${projectId}&country=US&subdivision=NY`,
+      `${onRampPath}/buy/options?projectId=${projectId}&country=${country}&subdivision=${subdivision}`,
     )
     expect(resp.status).toBe(200)
     
@@ -26,5 +28,31 @@ describe('OnRamp', () => {
     expect(typeof firstPurchaseNetworks.displayName).toBe('string')
     expect(typeof firstPurchaseNetworks.contractAddress).toBe('string')
     expect(typeof firstPurchaseNetworks.chainId).toBe('string')
+  })
+
+  it('buy quotes', async () => {
+    let resp: any = await httpClient.get(
+      `${onRampPath}/buy/quotes` +
+      `?projectId=${projectId}` +
+      `&country=${country}` +
+      `&subdivision=${subdivision}` +
+      `&purchaseCurrency=BCH` +
+      `&paymentAmount=100.00` +
+      `&paymentCurrency=USD` +
+      `&paymentMethod=CARD`,
+    );
+    expect(resp.status).toBe(200)
+
+    const checkValueAndCurrency = (obj: any) => {
+      expect(typeof obj.value).toBe('string')
+      expect(typeof obj.currency).toBe('string')
+    }
+    
+    expect(typeof resp.data.quoteId).toBe('string')
+    checkValueAndCurrency(resp.data.paymentTotal)
+    checkValueAndCurrency(resp.data.paymentSubTotal)
+    checkValueAndCurrency(resp.data.purchaseAmount)
+    checkValueAndCurrency(resp.data.coinbaseFee)
+    checkValueAndCurrency(resp.data.networkFee)
   })
 })
