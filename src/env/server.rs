1a65c1fd7136d2f5c3eac12e6a2934f6bf8350f8
Max Kalashnikoff <geekmaks@gmail.com>
2024-02-19 17:53:47 +0100

    feat(config): config parameter to disable project id check

--- src/env/server.rs
+++ src/env/server.rs
@@ -17,6 +17,7 @@ pub struct ServerConfig {
     pub geoip_db_bucket: Option<String>,
     pub geoip_db_key: Option<String>,
     pub testing_project_id: Option<String>,
+    pub validate_project_id: bool,
 }
 
 impl Default for ServerConfig {
@@ -32,6 +33,7 @@ impl Default for ServerConfig {
             geoip_db_bucket: None,
             geoip_db_key: None,
             testing_project_id: None,
+            validate_project_id: true,
         }
     }
 }
