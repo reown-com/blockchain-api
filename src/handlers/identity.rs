7a59595d6e696d82db818eda4b90eea2eba128d2
Max Kalashnikoff <geekmaks@gmail.com>
2024-03-14 18:59:46 +0100

    fix(identity): handling `0x` RPC response for identity (#582)
    * fix(bug): handling 0x RPC response for identity
    
    * fix: moving the 0x to the constant
    
    * fix: handling error on serde serialize
    
    * fix: moving to a separate error variant

--- src/handlers/identity.rs
+++ src/handlers/identity.rs
@@ -32,6 +32,8 @@ use {
     wc::future::FutureExt,
 };
 
+const EMPTY_RPC_RESPONSE: &str = "0x";
+
 #[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Hash, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct IdentityResponse {
@@ -325,6 +327,9 @@ pub enum SelfProviderError {
 
     #[error("JsonRpcError: {0:?}")]
     JsonRpcError(JsonRpcError),
+
+    #[error("Generic parameter error: {0}")]
+    GenericParameterError(String),
 }
 
 impl ethers::providers::RpcError for SelfProviderError {
@@ -398,10 +403,24 @@ impl JsonRpcClient for SelfProvider {
 
         let result = match response {
             JsonRpcResponse::Error(e) => return Err(SelfProviderError::JsonRpcError(e)),
-            JsonRpcResponse::Result(r) => r.result,
+            JsonRpcResponse::Result(r) => {
+                // We shouldn't process with `0x` result because this leads to the ethers-rs
+                // panic when looking for an avatar
+                if r.result == EMPTY_RPC_RESPONSE {
+                    return Err(SelfProviderError::ProviderError {
+                        status: StatusCode::METHOD_NOT_ALLOWED,
+                        body: format!("JSON-RPC result is {}", EMPTY_RPC_RESPONSE),
+                    });
+                } else {
+                    r.result
+                }
+            }
         };
-        let result = serde_json::from_value(result)
-            .expect("Caller always provides generic parameter R=Bytes");
+        let result = serde_json::from_value(result).map_err(|_| {
+            SelfProviderError::GenericParameterError(
+                "Caller always provides generic parameter R=Bytes".into(),
+            )
+        })?;
         Ok(result)
     }
 }
