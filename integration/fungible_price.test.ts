fa42400a1363c6310b5e02a958375d52944af92e
Max Kalashnikoff <geekmaks@gmail.com>
2024-04-05 13:10:41 +0200

    feat: integration tests update

--- integration/fungible_price.test.ts
+++ integration/fungible_price.test.ts
@@ -2,14 +2,21 @@ import { getTestSetup } from './init';
 
 describe('Fungible price', () => {
   const { baseUrl, projectId, httpClient } = getTestSetup();
+  const endpoint = `${baseUrl}/v1/fungible/price`;
 
   // RSR token address
   const implementation_address = 'eip155:1:0x320623b8e4ff03373931769a31fc52a4e78b5d70'
   const currency = 'usd'
 
   it('get token price', async () => {
-    let resp: any = await httpClient.get(
-      `${baseUrl}/v1/fungible/price?projectId=${projectId}&currency=${currency}&address=${implementation_address}`
+    let request_data = {
+      projectId: projectId,
+      currency: currency,
+      addresses: [implementation_address]
+    }
+    let resp: any = await httpClient.post(
+      `${endpoint}`,
+      request_data
     )
     expect(resp.status).toBe(200)
     expect(typeof resp.data.fungibles).toBe('object')
@@ -21,5 +28,32 @@ describe('Fungible price', () => {
       expect(typeof item.iconUrl).toBe('string')
       expect(typeof item.price).toBe('number')
     }
+  })
+
+  it('bad arguments', async () => {
+    // Empty addresses
+    let request_data = {
+      projectId: projectId,
+      currency: currency,
+      addresses: []
+    }
+    let resp: any = await httpClient.post(
+      `${endpoint}`,
+      request_data
+    )
+    expect(resp.status).toBe(400)
+
+    // Wrong currency
+    request_data = {
+      projectId: projectId,
+      currency: "irn",
+      addresses: [implementation_address]
+    }
+    resp = await httpClient.post(
+      `${endpoint}`,
+      request_data
+    )
+    expect(resp.status).toBe(422)
+    
   })
 })
