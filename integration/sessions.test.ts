aab0f7ed941e32ccf44565fbd207c1edd2c5557f
Max Kalashnikoff <geekmaks@gmail.com>
2024-06-25 18:58:30 +0200

    feat: integration tests

---
+++ integration/sessions.test.ts
@@ -0,0 +1,50 @@
+import { getTestSetup } from './init';
+import { ethers } from "ethers"
+
+describe('Sessions/Permissions', () => {
+  const { baseUrl, projectId, httpClient } = getTestSetup();
+  const address = ethers.Wallet.createRandom().address;
+  // Session payload
+  const payload = {
+    permissions: {
+      permissionType: "exampleType",
+      data: "exampleData",
+      required: true,
+      onChainValidated: false
+    }
+  }
+  let new_pci: string;
+  
+  it('create new session', async () => {
+    let resp: any = await httpClient.post(
+      `${baseUrl}/v1/sessions/${address}`,
+      payload
+    )
+    expect(resp.status).toBe(200)
+    expect(typeof resp.data.pci).toBe('string')
+    new_pci = resp.data.pci
+    expect(typeof resp.data.key).toBe('string')
+    // check key is base64 encoded
+    expect(Buffer.from(resp.data.key, 'base64').toString('base64')).toBe(resp.data.key)
+  })
+
+  it('list PCIs for address', async () => {
+    let resp = await httpClient.get(
+      `${baseUrl}/v1/sessions/${address}`
+    )
+    expect(resp.status).toBe(200)
+    expect(resp.data.pci.length).toBe(1)
+    expect(resp.data.pci[0]).toBe(new_pci)
+  })
+
+  it('get session by PCI', async () => {
+    let resp = await httpClient.get(
+      `${baseUrl}/v1/sessions/${address}/${new_pci}`
+    )
+    expect(resp.status).toBe(200)
+    expect(resp.data.permissionType).toBe(payload.permissions.permissionType)
+    expect(resp.data.data).toBe(payload.permissions.data)
+    expect(resp.data.required).toBe(payload.permissions.required)
+    expect(resp.data.onChainValidated).toBe(payload.permissions.onChainValidated)
+  })
+})
