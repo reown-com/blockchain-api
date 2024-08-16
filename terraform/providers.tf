916c45d9aa7898eec5b97d5e7b0dee8610e633bf
Chris Smith <chris@walletconnect.com>
2024-08-16 13:56:38 -0400

    feat: multi-region deployment

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,5 +1,24 @@
 provider "aws" {
+  alias  = "eu-central-1"
   region = var.region
+
+  default_tags {
+    tags = module.this.tags
+  }
+}
+
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
+
+  default_tags {
+    tags = module.this.tags
+  }
+}
+
+provider "aws" {
+  alias  = "ap-southeast-1"
+  region = "ap-southeast-1"
 
   default_tags {
     tags = module.this.tags
