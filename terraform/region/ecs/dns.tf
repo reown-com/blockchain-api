cab7842a37a276a73c4668f3abcab580fc8d28cd
Chris Smith <chris@walletconnect.com>
2024-08-26 15:05:04 -0700

    chore: regional endpoints

--- terraform/region/ecs/dns.tf
+++ terraform/region/ecs/dns.tf
@@ -1,4 +1,18 @@
 # DNS Records
+resource "aws_route53_record" "dns_load_balancer_region" {
+  for_each = var.route53_zones
+
+  zone_id = each.key
+  name    = "${module.this.region}.${each.value}"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.load_balancer.dns_name
+    zone_id                = aws_lb.load_balancer.zone_id
+    evaluate_target_health = true
+  }
+}
+
 resource "aws_route53_record" "dns_load_balancer" {
   for_each = var.route53_zones
 
@@ -12,3 +26,16 @@ resource "aws_route53_record" "dns_load_balancer" {
     evaluate_target_health = true
   }
 }
+
+# resource "aws_route53_health_check" "health_check" {
+#   fqdn              = local.fqdn
+#   port              = 443
+#   type              = "HTTPS"
+#   resource_path     = "/health"
+#   failure_threshold = "5"
+#   request_interval  = "30"
+
+#   tags = {
+#     Name = "${var.environment}.${var.region}.${var.app_name}-health-check"
+#   }
+# }
--- terraform/region/res_dns.tf
+++ terraform/region/res_dns.tf
@@ -1,6 +1,18 @@
 locals {
   zones              = { for k, v in tomap(data.terraform_remote_state.infra_aws.outputs.zones.blockchain[local.stage]) : v.id => v.name }
-  zones_certificates = { for k, v in module.dns_certificate : v.zone_id => v.certificate_arn }
+  zones_certificates = merge(
+    { for k, v in module.dns_certificate : v.zone_id => v.certificate_arn },
+    { for k, v in module.dns_certificate_region : v.zone_id => v.certificate_arn }
+  )
+}
+
+module "dns_certificate_region" {
+  for_each         = local.zones
+  source           = "app.terraform.io/wallet-connect/dns/aws"
+  version          = "0.1.3"
+  context          = module.this
+  hosted_zone_name = each.value
+  fqdn             = "${module.this.region}.${each.value}"
 }
 
 module "dns_certificate" {
