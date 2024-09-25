e9e364f2c67443a000970d1d83df1c9be3e9fe43
Chris Smith <chris@walletconnect.com>
2024-09-25 15:36:42 -0400

    feat: implement dummy signature and paymaster

--- src/handlers/wallet/prepare_calls.rs
+++ src/handlers/wallet/prepare_calls.rs
@@ -2,8 +2,10 @@ use crate::analytics::MessageSource;
 use crate::{handlers::HANDLER_TASK_METRICS, state::AppState};
 use alloy::network::Ethereum;
 use alloy::primitives::aliases::U192;
-use alloy::primitives::{address, Address, Bytes, U256, U64};
+use alloy::primitives::{address, bytes, Address, Bytes, FixedBytes, U256, U64};
 use alloy::providers::ReqwestProvider;
+use alloy::sol_types::SolCall;
+use alloy::sol_types::SolValue;
 use axum::{
     extract::State,
     response::{IntoResponse, Response},
@@ -15,15 +17,14 @@ use std::sync::Arc;
 use thiserror::Error;
 use tracing::error;
 use wc::future::FutureExt;
+use yttrium::erc7579::smart_sessions::ISmartSession::isSessionEnabledReturn;
+use yttrium::erc7579::smart_sessions::{enableSessionSigCall, EnableSession, ISmartSession};
 use yttrium::smart_accounts::account_address::AccountAddress;
 use yttrium::{
     bundler::{config::BundlerConfig, pimlico::client::BundlerClient},
     chain::ChainId,
     entry_point::{EntryPointConfig, EntryPointVersion},
-    smart_accounts::{
-        nonce::get_nonce_with_key,
-        safe::{get_call_data, DUMMY_SIGNATURE},
-    },
+    smart_accounts::{nonce::get_nonce_with_key, safe::get_call_data},
     transaction::Transaction,
     user_operation::{user_operation_hash::UserOperationHash, UserOperationV07},
 };
@@ -89,8 +90,23 @@ pub enum PrepareCallsError {
     #[error("Invalid chain ID")]
     InvalidChainId,
 
-    #[error("Permission context doesn't start with address")]
-    PermissionContextDoesntStartWithAddress,
+    #[error("Permission context not long enough")]
+    PermissionContextNotLongEnough,
+
+    #[error("Permission context signature decompression error: {0}")]
+    PermissionContextSignatureDecompression(fastlz_rs::DecompressError),
+
+    #[error("Unsupported permission context mode: USE")]
+    PermissionContextUnsupportedModeUse,
+
+    #[error("Invalid permission context mode")]
+    PermissionContextInvalidMode,
+
+    #[error("Permission context ABI decode: {0}")]
+    PermissionContextAbiDecode(alloy::sol_types::Error),
+
+    #[error("Invalid permissionEnableSig for kernel account")]
+    PermissionContextInvalidPermissionEnableSigForKernelAccount,
 
     #[error("Invalid permission context")]
     InvalidPermissionContext,
@@ -106,6 +122,12 @@ pub enum PrepareCallsInternalError {
 
     #[error("Estimate user operation gas price: {0}")]
     EstimateUserOperationGasPrice(eyre::Error),
+
+    #[error("isSessionEnabled: {0}")]
+    IsSessionEnabled(alloy::contract::Error),
+
+    #[error("Compress session enabled: {0}")]
+    CompressSessionEnabled(fastlz_rs::CompressError),
 }
 
 impl IntoResponse for PrepareCallsError {
@@ -156,6 +178,8 @@ async fn handler_internal(
 
         // TODO get version from contract: https://github.com/reown-com/web-examples/blob/32f9df464e2fa85ec49c21837d811cfe1437719e/advanced/wallets/react-wallet-v2/src/lib/smart-accounts/builders/SafeUserOpBuilder.ts#L65
 
+        let account_type = AccountType::Safe;
+
         let entry_point_config = EntryPointConfig {
             chain_id,
             version: EntryPointVersion::V07,
@@ -172,26 +196,122 @@ async fn handler_internal(
             .unwrap(),
         );
 
-        let nonce = {
-            // https://github.com/rhinestonewtf/module-sdk/blob/18ef7ca998c0d0a596572f18575e1b4967d9227b/src/module/smart-sessions/constants.ts#L2
-            const SMART_SESSIONS_ADDRESS: Address =
-                address!("82e5e20582d976f5db5e36c5a72c70d5711cef8b");
+        // https://github.com/rhinestonewtf/module-sdk/blob/18ef7ca998c0d0a596572f18575e1b4967d9227b/src/module/smart-sessions/constants.ts#L2
+        const SMART_SESSIONS_ADDRESS: Address =
+            address!("82e5e20582d976f5db5e36c5a72c70d5711cef8b");
 
-            let validator_address = validator_address_from_permission_context(
-                &request.capabilities.permissions.context,
-            )?;
-            if validator_address != SMART_SESSIONS_ADDRESS {
-                return Err(PrepareCallsError::InvalidPermissionContext);
-            }
+        let (validator_address, signature) = request
+            .capabilities
+            .permissions
+            .context
+            .split_at_checked(20)
+            .ok_or(PrepareCallsError::PermissionContextNotLongEnough)?;
 
-            // https://github.com/reown-com/web-examples/blob/32f9df464e2fa85ec49c21837d811cfe1437719e/advanced/wallets/react-wallet-v2/src/lib/smart-accounts/builders/SafeUserOpBuilder.ts#L110
-            let key = key_from_validator_address(validator_address);
-            get_nonce_with_key(&provider, request.from, &entry_point_config.address(), key)
+        let validator_address = Address::from_slice(validator_address);
+        if validator_address != SMART_SESSIONS_ADDRESS {
+            return Err(PrepareCallsError::InvalidPermissionContext);
+        }
+
+        let dummy_signature = {
+            let DecodedSmartSessionSignature {
+                permission_id,
+                enable_session_data,
+            } = decode_smart_session_signature(signature, account_type)?;
+
+            const DUMMY_ECDSA_SIGNATURE: Bytes = bytes!("e8b94748580ca0b4993c9a1b86b5be851bfc076ff5ce3a1ff65bf16392acfcb800f9b4f1aef1555c7fce5599fffb17e7c635502154a0333ba21f3ae491839af51c");
+            let signature = decode_signers(
+                enable_session_data
+                    .enable_session
+                    .sessionToEnable
+                    .sessionValidatorInitData
+                    .clone(),
+            )?
+            .into_iter()
+            .map(|t| match t {
+                SignerType::Ecdsa => DUMMY_ECDSA_SIGNATURE,
+                SignerType::Passkey => bytes!(""),
+            })
+            .collect::<Vec<_>>()
+            .abi_encode();
+
+            let smart_sessions = ISmartSession::new(SMART_SESSIONS_ADDRESS, provider.clone());
+            let isSessionEnabledReturn {
+                _0: session_enabled,
+            } = smart_sessions
+                .isSessionEnabled(permission_id, request.from.into())
+                .call()
                 .await
                 .map_err(|e| {
-                    PrepareCallsError::InternalError(PrepareCallsInternalError::GetNonce(e))
-                })?
+                    PrepareCallsError::InternalError(PrepareCallsInternalError::IsSessionEnabled(e))
+                })?;
+            if session_enabled {
+                let mut compress_state = fastlz_rs::CompressState::new();
+                let compressed = Bytes::from(
+                    compress_state
+                        .compress_to_vec(&signature, fastlz_rs::CompressionLevel::Default)
+                        .map_err(|e| {
+                            PrepareCallsError::InternalError(
+                                PrepareCallsInternalError::CompressSessionEnabled(e),
+                            )
+                        })?,
+                );
+                (Bytes::from([MODE_USE]), permission_id, compressed)
+                    .abi_encode_packed()
+                    .into()
+            } else {
+                let signature = (
+                    enable_session_data.enable_session,
+                    // EnableSession {
+                    //     chainDigestIndex: enable_session_data.enable_session.chainDigestIndex,
+                    //     hashesAndChainIds: enable_session_data.enable_session.hashesAndChainIds,
+                    //     sessionToEnable: enable_session_data.enable_session.sessionToEnable,
+                    //     permissionEnableSig: match account_type {
+                    //         AccountType::Erc7579Implementation
+                    //         | AccountType::Safe
+                    //         | AccountType::Nexus => (
+                    //             enable_session_data.validator,
+                    //             enable_session_data.enable_session.permissionEnableSig,
+                    //         )
+                    //             .abi_encode_packed()
+                    //             .into(),
+                    //         AccountType::Kernel => (
+                    //             [0x01],
+                    //             enable_session_data.validator,
+                    //             enable_session_data.enable_session.permissionEnableSig,
+                    //         )
+                    //             .abi_encode_packed()
+                    //             .into(),
+                    //     },
+                    // },
+                    signature,
+                )
+                    .abi_encode();
+
+                let mut compress_state = fastlz_rs::CompressState::new();
+                let compressed = Bytes::from(
+                    compress_state
+                        .compress_to_vec(&signature, fastlz_rs::CompressionLevel::Default)
+                        .map_err(|e| {
+                            PrepareCallsError::InternalError(
+                                PrepareCallsInternalError::CompressSessionEnabled(e),
+                            )
+                        })?,
+                );
+                (Bytes::from([MODE_ENABLE]), permission_id, compressed)
+                    .abi_encode_packed()
+                    .into()
+            }
         };
+
+        // https://github.com/reown-com/web-examples/blob/32f9df464e2fa85ec49c21837d811cfe1437719e/advanced/wallets/react-wallet-v2/src/lib/smart-accounts/builders/SafeUserOpBuilder.ts#L110
+        let nonce = get_nonce_with_key(
+            &provider,
+            request.from,
+            &entry_point_config.address(),
+            key_from_validator_address(validator_address),
+        )
+        .await
+        .map_err(|e| PrepareCallsError::InternalError(PrepareCallsInternalError::GetNonce(e)))?;
 
         let pimlico_client = BundlerClient::new(BundlerConfig::new(
             format!(
@@ -211,9 +331,6 @@ async fn handler_internal(
                 )
             })?;
 
-        // TODO prepare paymaster info
-        // TODO get special dummy signature: https://github.com/reown-com/web-examples/blob/32f9df464e2fa85ec49c21837d811cfe1437719e/advanced/wallets/react-wallet-v2/src/lib/smart-accounts/builders/ContextBuilderUtil.ts#L190
-
         let user_operation = UserOperationV07 {
             sender: request.from,
             nonce,
@@ -229,7 +346,7 @@ async fn handler_internal(
             paymaster_verification_gas_limit: None,
             paymaster_post_op_gas_limit: None,
             paymaster_data: None,
-            signature: DUMMY_SIGNATURE,
+            signature: dummy_signature,
         };
         let hash = user_operation.hash(
             &entry_point_config.address().to_address(),
@@ -258,12 +375,129 @@ fn key_from_validator_address(validator_address: Address) -> U192 {
     })
 }
 
-fn validator_address_from_permission_context(
-    context: &Bytes,
-) -> Result<Address, PrepareCallsError> {
-    Ok(Address::from_slice(context.get(0..20).ok_or(
-        PrepareCallsError::PermissionContextDoesntStartWithAddress,
-    )?))
+// https://github.com/rhinestonewtf/module-sdk/blob/18ef7ca998c0d0a596572f18575e1b4967d9227b/src/account/types.ts#L4
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
+enum AccountType {
+    #[serde(rename = "erc7579-implementation")]
+    Erc7579Implementation,
+
+    #[serde(rename = "kernel")]
+    Kernel,
+
+    #[serde(rename = "safe")]
+    Safe,
+
+    #[serde(rename = "nexus")]
+    Nexus,
+}
+
+struct EnableSessionData {
+    enable_session: EnableSession,
+    // validator: Address,
+}
+
+struct DecodedSmartSessionSignature {
+    permission_id: FixedBytes<32>,
+    enable_session_data: EnableSessionData,
+}
+
+// https://github.com/rhinestonewtf/module-sdk/blob/18ef7ca998c0d0a596572f18575e1b4967d9227b/src/module/smart-sessions/types.ts#L42
+const MODE_USE: u8 = 0x00;
+const MODE_ENABLE: u8 = 0x01;
+const MODE_UNSAFE_ENABLE: u8 = 0x02;
+
+// https://github.com/rhinestonewtf/module-sdk/blob/18ef7ca998c0d0a596572f18575e1b4967d9227b/src/module/smart-sessions/usage.ts#L209
+fn decode_smart_session_signature(
+    signature: &[u8],
+    _account_type: AccountType,
+) -> Result<DecodedSmartSessionSignature, PrepareCallsError> {
+    let mode = signature
+        .first()
+        .ok_or(PrepareCallsError::PermissionContextNotLongEnough)?;
+    let permission_id = signature
+        .get(1..33)
+        .ok_or(PrepareCallsError::PermissionContextNotLongEnough)?
+        .try_into() // this error shouldn't happen
+        .map_err(|_| PrepareCallsError::PermissionContextNotLongEnough)?;
+    let compressed_data = signature
+        .get(33..)
+        .ok_or(PrepareCallsError::PermissionContextNotLongEnough)?;
+
+    let data = fastlz_rs::decompress_to_vec(compressed_data, None)
+        .map_err(PrepareCallsError::PermissionContextSignatureDecompression)?;
+
+    match *mode {
+        MODE_USE => {
+            // https://github.com/rhinestonewtf/module-sdk/blob/18ef7ca998c0d0a596572f18575e1b4967d9227b/src/module/smart-sessions/usage.ts#L221
+            // We aren't implementing this currently because it doesn't return the needed value (enableSessionData)
+            Err(PrepareCallsError::PermissionContextUnsupportedModeUse)
+        }
+        MODE_ENABLE | MODE_UNSAFE_ENABLE => {
+            let enableSessionSigCall {
+                session: enable_session,
+                signature: _,
+            } = enableSessionSigCall::abi_decode_raw(&data, true)
+                .map_err(PrepareCallsError::PermissionContextAbiDecode)?;
+            // let is_kernel = account_type == AccountType::Kernel;
+            // if is_kernel && enable_session.permissionEnableSig.starts_with(&[0x01]) {
+            //     return Err(
+            //         PrepareCallsError::PermissionContextInvalidPermissionEnableSigForKernelAccount,
+            //     );
+            // }
+
+            // let permission_enable_sig =
+            //     &enable_session.permissionEnableSig[if is_kernel { 1 } else { 0 }..];
+            // let (validator, permission_enable_sig) = permission_enable_sig
+            //     .split_at_checked(20)
+            //     .ok_or(PrepareCallsError::PermissionContextNotLongEnough)?;
+            // let validator = Address::from_slice(validator);
+
+            Ok(DecodedSmartSessionSignature {
+                permission_id,
+                enable_session_data: EnableSessionData {
+                    enable_session,
+                    // enable_session: EnableSession {
+                    //     chainDigestIndex: enable_session.chainDigestIndex,
+                    //     hashesAndChainIds: enable_session.hashesAndChainIds,
+                    //     sessionToEnable: enable_session.sessionToEnable,
+                    //     permissionEnableSig: permission_enable_sig.into(), // TODO skip all this and just pass-through as-is
+                    // },
+                    // validator,
+                },
+            })
+        }
+        _ => Err(PrepareCallsError::PermissionContextInvalidMode),
+    }
+}
+
+enum SignerType {
+    Ecdsa,
+    Passkey,
+}
+
+fn decode_signers(data: Bytes) -> Result<Vec<SignerType>, PrepareCallsError> {
+    let mut data = data.into_iter();
+    let signer_count = data
+        .next()
+        .ok_or(PrepareCallsError::InvalidPermissionContext)?; // TODO correct error variants
+    let mut signers = Vec::with_capacity(signer_count as usize);
+    for _i in 0..signer_count {
+        let (signer_type, length) = match data.next() {
+            Some(0) => (SignerType::Ecdsa, 20),
+            Some(1) => (SignerType::Passkey, 64),
+            _ => return Err(PrepareCallsError::InvalidPermissionContext), // TODO correct error variants
+        };
+        // ignore the actual signature
+        for _i in 0..length {
+            data.next()
+                .ok_or(PrepareCallsError::InvalidPermissionContext)?; // TODO correct error variants
+        }
+        signers.push(signer_type);
+    }
+    if data.next().is_some() {
+        return Err(PrepareCallsError::InvalidPermissionContext); // TODO correct error variants
+    }
+    Ok(signers)
 }
 
 #[cfg(test)]
@@ -279,27 +513,5 @@ mod tests {
             key.to_be_bytes_vec(),
             bytes!("abababababababababababababababababababab00000000").to_vec()
         );
-    }
-
-    #[test]
-    fn test_validator_address_from_permission_context() {
-        assert_eq!(
-            validator_address_from_permission_context(&bytes!(
-                "abababababababababababababababababababab"
-            ))
-            .unwrap(),
-            address!("abababababababababababababababababababab")
-        );
-        assert_eq!(
-            validator_address_from_permission_context(&bytes!(
-                "ababababababababababababababababababababff"
-            ))
-            .unwrap(),
-            address!("abababababababababababababababababababab")
-        );
-        assert!(validator_address_from_permission_context(&bytes!(
-            "ababababababababababababababababababab"
-        ))
-        .is_err());
     }
 }
