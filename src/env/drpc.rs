a4b85085166e8f80af83c1c0c2d8f216b121fa92
Max Kalashnikoff <geekmaks@gmail.com>
2025-01-30 17:47:40 +0100

    feat: adding EVM chains

--- src/env/drpc.rs
+++ src/env/drpc.rs
@@ -34,6 +34,70 @@ impl ProviderConfig for DrpcConfig {
 fn default_supported_chains() -> HashMap<String, (String, Weight)> {
     // Keep in-sync with SUPPORTED_CHAINS.md
 
-    // TODO: Add EVM only chains
-    HashMap::new()
+    HashMap::from([
+        // Ethereum Mainnet
+        (
+            "eip155:1".into(),
+            (
+                "https://eth.drpc.org/".into(),
+                Weight::new(Priority::Normal).unwrap(),
+            ),
+        ),
+        // Ethereum Sepolia
+        (
+            "eip155:11155111".into(),
+            (
+                "https://sepolia.drpc.org".into(),
+                Weight::new(Priority::Normal).unwrap(),
+            ),
+        ),
+        // Ethereum Holesky
+        (
+            "eip155:17000".into(),
+            (
+                "https://holesky.drpc.org".into(),
+                Weight::new(Priority::Normal).unwrap(),
+            ),
+        ),
+        // Arbitrum One
+        (
+            "eip155:42161".into(),
+            (
+                "https://arbitrum.drpc.org".into(),
+                Weight::new(Priority::Normal).unwrap(),
+            ),
+        ),
+        // Base
+        (
+            "eip155:8453".into(),
+            (
+                "https://base.drpc.org".into(),
+                Weight::new(Priority::Normal).unwrap(),
+            ),
+        ),
+        // BSC
+        (
+            "eip155:56".into(),
+            (
+                "https://bsc.drpc.org".into(),
+                Weight::new(Priority::Normal).unwrap(),
+            ),
+        ),
+        // Polygon
+        (
+            "eip155:137".into(),
+            (
+                "https://polygon.drpc.org".into(),
+                Weight::new(Priority::Normal).unwrap(),
+            ),
+        ),
+        // Optimism
+        (
+            "eip155:10".into(),
+            (
+                "https://optimism.drpc.org".into(),
+                Weight::new(Priority::Normal).unwrap(),
+            ),
+        ),
+    ])
 }
