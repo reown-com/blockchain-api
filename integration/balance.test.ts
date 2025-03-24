229bfc6ca49996c230750fbe05907c05e19d9a27
Max Kalashnikoff | maksy.eth <geekmaks@gmail.com>
2025-03-24 21:21:30 +0100

    fix(balances): adding an exclusion for the affected SDK version (#1003)
    * fix(balances): adding an exclusion for affected sdk version
    
    * fix: adding each sdk version in test

--- integration/balance.test.ts
+++ integration/balance.test.ts
@@ -67,7 +67,7 @@ describe('Account balance', () => {
     }
   })
 
-  it('fulfilled balance Ethereum address no version header', async () => {
+  it('empty balance Ethereum address no version header', async () => {
     let resp: any = await httpClient.get(
       `${baseUrl}/v1/account/${fulfilled_eth_address}/balance?projectId=${projectId}&currency=${currency}`
     )
@@ -76,6 +76,19 @@ describe('Account balance', () => {
     expect(resp.status).toBe(200)
     expect(typeof resp.data.balances).toBe('object')
     expect(resp.data.balances).toHaveLength(0)
+  })
+
+  it('empty balance Ethereum address affected SDK version', async () => {
+    const affected_sdk_versions = ['1.6.4', '1.6.5']
+    for (const affected_sdk_version of affected_sdk_versions) {
+      let resp: any = await httpClient.get(
+        `${baseUrl}/v1/account/${fulfilled_eth_address}/balance?projectId=${projectId}&currency=${currency}&sv=${affected_sdk_version}`
+      )
+      // We should expect the empty balance response for the affected SDK version
+      expect(resp.status).toBe(200)
+      expect(typeof resp.data.balances).toBe('object')
+      expect(resp.data.balances).toHaveLength(0)
+    }
   })
 
   it('empty balance Ethereum address', async () => {
