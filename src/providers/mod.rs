e062049b67c6e14dd2bf6ad8fb854f0f988c7acd
Max Kalashnikoff | maksy.eth <geekmaks@gmail.com>
2025-04-15 21:21:52 +0200

    fix(debug): adding debug message for the balance provider weights (#1038)

--- src/providers/mod.rs
+++ src/providers/mod.rs
@@ -58,7 +58,7 @@ use {
         hash::Hash,
         sync::Arc,
     },
-    tracing::{debug, error, log::warn},
+    tracing::{debug, error, info, log::warn},
     wc::metrics::TaskMetrics,
     yttrium::chain_abstraction::api::Transaction,
 };
@@ -645,6 +645,10 @@ impl ProviderRepository {
                 warn!("Failed to update weights from prometheus: {}", e);
             }
         }
+        info!(
+            "Balance providers weights: {:?}",
+            self.balance_weight_resolver
+        );
     }
 
     #[tracing::instrument(skip(self), level = "debug")]
