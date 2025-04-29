653bf191a566216ca3054c630b991814e7aabc17
Max Kalashnikoff | maksy.eth <geekmaks@gmail.com>
2025-04-29 11:08:35 +0200

    fix(CI): removing unsupported bundler test (#1048)

--- integration/bundler.test.ts
+++ integration/bundler.test.ts
@@ -7,21 +7,6 @@ describe('Bundler operations', () => {
   const mainnetChainId = 'eip155:1'
   const method = 'eth_getUserOperationReceipt'
   const successOperationTxHash = '0x772b10c68cb2470259be889b97e87618a4d8fc2b21767503724a9842bc83b5de'
-  const bundler = 'pimlico'
-
-  it('unsupported bundler', async () => {
-    let json_rpc = {
-      jsonrpc: '2.0',
-      method,
-      params: [successOperationTxHash],
-      id: 1
-    }
-    let resp: any = await httpClient.post(
-      `${baseUrl}/v1/bundler?projectId=${projectId}&chainId=${sepoliaChainId}&bundler=test`,
-      json_rpc
-    )
-    expect(resp.status).toBe(400)
-  })
 
   it('unsupported method', async () => {
     let json_rpc = {
@@ -31,7 +16,7 @@ describe('Bundler operations', () => {
       id: 1
     }
     let resp: any = await httpClient.post(
-      `${baseUrl}/v1/bundler?projectId=${projectId}&chainId=${sepoliaChainId}&bundler=${bundler}`,
+      `${baseUrl}/v1/bundler?projectId=${projectId}&chainId=${sepoliaChainId}`,
       json_rpc
     )
     expect(resp.status).toBe(422)
@@ -45,7 +30,7 @@ describe('Bundler operations', () => {
       id: 1
     }
     let resp: any = await httpClient.post(
-      `${baseUrl}/v1/bundler?projectId=${projectId}&chainId=${mainnetChainId}&bundler=${bundler}`,
+      `${baseUrl}/v1/bundler?projectId=${projectId}&chainId=${mainnetChainId}`,
       json_rpc
     )
     expect(resp.status).toBe(200)
@@ -61,7 +46,7 @@ describe('Bundler operations', () => {
       id: 1
     }
     let resp: any = await httpClient.post(
-      `${baseUrl}/v1/bundler?projectId=${projectId}&chainId=${sepoliaChainId}&bundler=${bundler}`,
+      `${baseUrl}/v1/bundler?projectId=${projectId}&chainId=${sepoliaChainId}`,
       json_rpc
     )
     expect(resp.status).toBe(200)
