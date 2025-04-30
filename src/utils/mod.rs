8b8ead9a51f88033a397c223ca393253411a856d
Chris Smith <1979423+chris13524@users.noreply.github.com>
2025-04-30 13:37:48 -0400

    fix: highest balance check (#1015)

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -10,6 +10,7 @@ pub mod permissions;
 pub mod rate_limit;
 pub mod sessions;
 pub mod simple_request_json;
+pub mod token_amount;
 
 pub fn generate_random_string(len: usize) -> String {
     let rng = rand::thread_rng();
