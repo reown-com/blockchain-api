105ec6c1f54d7246221a8c54b6316b2a2c365b17
jackkru69 <www.jackkru69@mail.ru>
2025-07-19 02:25:03 +0300

    feat(tests): add functional tests for DeInfra provider

---
+++ tests/functional/http/deinfra.rs
@@ -0,0 +1,29 @@
+use {
+    super::check_if_rpc_is_responding_correctly_for_supported_chain, crate::context::ServerContext,
+    rpc_proxy::providers::ProviderKind, test_context::test_context,
+};
+
+#[test_context(ServerContext)]
+#[tokio::test]
+#[ignore]
+async fn deinfra_provider(ctx: &mut ServerContext) {
+    let provider_kind = ProviderKind::DeInfra;
+
+    // DeInfra Mainnet
+    check_if_rpc_is_responding_correctly_for_supported_chain(
+        ctx,
+        &provider_kind,
+        "eip155:100501",
+        "0x188f5",
+    )
+    .await;
+
+    // DeInfra Devnet3
+    check_if_rpc_is_responding_correctly_for_supported_chain(
+        ctx,
+        &provider_kind,
+        "eip155:1000000003",
+        "0x3b9acae3",
+    )
+    .await;
+}
