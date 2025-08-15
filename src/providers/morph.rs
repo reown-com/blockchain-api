80ac02dbfc3f866281ee42027cbb5e76d762fc89
Max Kalashnikoff <geekmaks@gmail.com>
2025-08-15 14:04:27 +0200

    fix: fixing fmt and license

--- src/providers/morph.rs
+++ src/providers/morph.rs
@@ -1,84 +1,84 @@
 use {
-	super::{Provider, ProviderKind, RateLimited, RpcProvider, RpcProviderFactory},
-	crate::{
-		env::MorphConfig,
-		error::{RpcError, RpcResult},
-	},
-	async_trait::async_trait,
-	axum::{
-		http::HeaderValue,
-		response::{IntoResponse, Response},
-	},
-	hyper::http,
-	std::collections::HashMap,
+    super::{Provider, ProviderKind, RateLimited, RpcProvider, RpcProviderFactory},
+    crate::{
+        env::MorphConfig,
+        error::{RpcError, RpcResult},
+    },
+    async_trait::async_trait,
+    axum::{
+        http::HeaderValue,
+        response::{IntoResponse, Response},
+    },
+    hyper::http,
+    std::collections::HashMap,
 };
 
 #[derive(Debug)]
 pub struct MorphProvider {
-	pub client: reqwest::Client,
-	pub supported_chains: HashMap<String, String>,
+    pub client: reqwest::Client,
+    pub supported_chains: HashMap<String, String>,
 }
 
 impl Provider for MorphProvider {
-	fn supports_caip_chainid(&self, chain_id: &str) -> bool {
-		self.supported_chains.contains_key(chain_id)
-	}
+    fn supports_caip_chainid(&self, chain_id: &str) -> bool {
+        self.supported_chains.contains_key(chain_id)
+    }
 
-	fn supported_caip_chains(&self) -> Vec<String> {
-		self.supported_chains.keys().cloned().collect()
-	}
+    fn supported_caip_chains(&self) -> Vec<String> {
+        self.supported_chains.keys().cloned().collect()
+    }
 
-	fn provider_kind(&self) -> ProviderKind {
-		ProviderKind::Morph
-	}
+    fn provider_kind(&self) -> ProviderKind {
+        ProviderKind::Morph
+    }
 }
 
 #[async_trait]
 impl RateLimited for MorphProvider {
-	async fn is_rate_limited(&self, response: &mut Response) -> bool {
-		response.status() == http::StatusCode::TOO_MANY_REQUESTS
-	}
+    async fn is_rate_limited(&self, response: &mut Response) -> bool {
+        response.status() == http::StatusCode::TOO_MANY_REQUESTS
+    }
 }
 
 #[async_trait]
 impl RpcProvider for MorphProvider {
-	#[tracing::instrument(skip(self, body), fields(provider = %self.provider_kind()), level = "debug")]
-	async fn proxy(&self, chain_id: &str, body: bytes::Bytes) -> RpcResult<Response> {
-		let uri = self
-			.supported_chains
-			.get(chain_id)
-			.ok_or(RpcError::ChainNotFound)?;
+    #[tracing::instrument(skip(self, body), fields(provider = %self.provider_kind()), level = "debug")]
+    async fn proxy(&self, chain_id: &str, body: bytes::Bytes) -> RpcResult<Response> {
+        let uri = self
+            .supported_chains
+            .get(chain_id)
+            .ok_or(RpcError::ChainNotFound)?;
 
-		let response = self
-			.client
-			.post(uri)
-			.header(reqwest::header::CONTENT_TYPE, "application/json")
-			.body(body)
-			.send()
-			.await?;
-		let status = response.status();
-		let body = response.bytes().await?;
-		let mut response = (status, body).into_response();
-		response
-			.headers_mut()
-			.insert("Content-Type", HeaderValue::from_static("application/json"));
-		Ok(response)
-	}
+        let response = self
+            .client
+            .post(uri)
+            .header(reqwest::header::CONTENT_TYPE, "application/json")
+            .body(body)
+            .send()
+            .await?;
+        let status = response.status();
+        let body = response.bytes().await?;
+        let mut response = (status, body).into_response();
+        response
+            .headers_mut()
+            .insert("Content-Type", HeaderValue::from_static("application/json"));
+        Ok(response)
+    }
 }
 
 impl RpcProviderFactory<MorphConfig> for MorphProvider {
-	#[tracing::instrument(level = "debug")]
-	fn new(provider_config: &MorphConfig) -> Self {
-		let forward_proxy_client = reqwest::Client::new();
-		let supported_chains: HashMap<String, String> = provider_config
-			.supported_chains
-			.iter()
-			.map(|(k, v)| (k.clone(), v.0.clone()))
-			.collect();
+    #[tracing::instrument(level = "debug")]
+    fn new(provider_config: &MorphConfig) -> Self {
+        let forward_proxy_client = reqwest::Client::new();
+        let supported_chains: HashMap<String, String> = provider_config
+            .supported_chains
+            .iter()
+            .map(|(k, v)| (k.clone(), v.0.clone()))
+            .collect();
 
-		MorphProvider {
-			client: forward_proxy_client,
-			supported_chains,
-		}
-	}
+        MorphProvider {
+            client: forward_proxy_client,
+            supported_chains,
+        }
+    }
 }
