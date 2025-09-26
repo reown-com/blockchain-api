8fc94816466cbfb00cf8b94c68cdc08173ddeff3
Luka Isailovic <lukaisailovic@gmail.com>
2025-09-26 19:19:11 +0200

    chore: add filter if no asset provided

--- src/handlers/json_rpc/exchanges/mod.rs
+++ src/handlers/json_rpc/exchanges/mod.rs
@@ -203,7 +203,7 @@ pub fn get_supported_exchanges(asset: Option<String>) -> Result<Vec<Exchange>, E
                 .map(|e| e.to_exchange())
                 .collect())
         }
-        None => Ok(ExchangeType::iter().map(|e| e.to_exchange()).collect()),
+        None => Ok(ExchangeType::iter().filter(|e| e.is_enabled()).map(|e| e.to_exchange()).collect()),
     }
 }
 
