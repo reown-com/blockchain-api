1813afece635bd2305f5d2c42f1a596ad503d0f0
Max Kalashnikoff | maksy.eth <geekmaks@gmail.com>
2025-10-02 11:17:30 +0200

    feat(rpc): wrapping Tron broadcast transaction API into the custom JSON-RPC method (#1258)
    * feat: wrapping Tron broadcast transaction method
    
    * fix: nits
    
    * fix: proper error handlng
    
    * fix: removing error message

--- integration/proxy.test.ts
+++ integration/proxy.test.ts
@@ -151,6 +151,32 @@ describe('Proxy', () => {
     }
   })
 
+  it('Tron broadcast transaction wrapped RPC method', async () => {
+    const chainId = "tron:0x2b6653dc";
+    // Expired transaction payload, but we can check if the method is working
+    const payload = {
+      jsonrpc: "2.0",
+      method: "tron_broadcastTransaction",
+      params:[
+        "0x1",
+        "true",
+        "{\"contract\":[{\"parameter\":{\"value\":{\"amount\":1000,\"owner_address\":\"41608f8da72479edc7dd921e4c30bb7e7cddbe722e\",\"to_address\":\"41e9d79cc47518930bc322d9bf7cddd260a0260a8d\"},\"type_url\":\"type.googleapis.com/protocol.TransferContract\"},\"type\":\"TransferContract\"}],\"ref_block_bytes\":\"5e4b\",\"ref_block_hash\":\"47c9dc89341b300d\",\"expiration\":1591089627000,\"timestamp\":1591089567635}",
+        "0a025e4b220847c9dc89341b300d40f8fed3a2a72e5a66080112620a2d747970652e676f6f676c65617069732e636f6d2f70726f746f636f6c2e5472616e73666572436f6e747261637412310a1541608f8da72479edc7dd921e4c30bb7e7cddbe722e121541e9d79cc47518930bc322d9bf7cddd260a0260a8d18e8077093afd0a2a72e",
+        ["0a025e4b220847c9dc8934"]
+      ],
+      id: 1,
+    };
+
+    const resp: any = await httpClient.post(
+      `${baseUrl}/v1?chainId=${chainId}&projectId=${projectId}`,
+      payload
+    )
+    expect(resp.status).toBe(200)
+    expect(typeof resp.data).toBe('object')
+    expect(typeof resp.data.result).toBe('object')
+    expect(typeof resp.data.result.txid).toBe('string')
+  })
+
   it('net_listening RPC method', async () => {
     // Get the chains list from the /supported-chains endpoint
     const chainsResp: any = await httpClient.get(`${baseUrl}/v1/supported-chains`)
