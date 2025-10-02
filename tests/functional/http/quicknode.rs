95131007bf955b4d97fc5ce4c746eda4c6c87978
Max Kalashnikoff | maksy.eth <geekmaks@gmail.com>
2025-10-02 15:41:14 +0200

    fix(RPC): removing Berachain bArtio chain (#1259)

--- tests/functional/http/quicknode.rs
+++ tests/functional/http/quicknode.rs
@@ -25,14 +25,6 @@ async fn quicknode_provider(ctx: &mut ServerContext) {
         "0x44d",
     )
     .await;
-    // Berachain Bartio
-    check_if_rpc_is_responding_correctly_for_supported_chain(
-        ctx,
-        &provider,
-        "eip155:80084",
-        "0x138d4",
-    )
-    .await;
 
     // Optimism Mainnet
     check_if_rpc_is_responding_correctly_for_supported_chain(ctx, &provider, "eip155:10", "0xa")
